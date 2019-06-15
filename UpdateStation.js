73deebbffdbbb0f7d0b15d2493dde414607f657b
svend <sven.depickere@student.howest.be>
2019-06-15 14:31:39 +0200

    Update UpdateStation.js

--- UpdateStation.js
+++ UpdateStation.js
@@ -13,7 +13,7 @@ function exportNewData(fileName, newData) {
     })
 }
 
-// removeTheWordStationFromTheTitle()
+// removeTheWordStationFromTheTitle();
 
 function removeTheWordStationFromTheTitle() {
     async function getStuff() {
