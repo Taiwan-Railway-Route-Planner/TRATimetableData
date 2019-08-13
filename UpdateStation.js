43060d9ed0106cfa065a85724f2c2e2d1777f1d2
svend <sven.depickere@student.howest.be>
2019-08-13 14:45:39 +0200

    refactor

--- UpdateStation.js
+++ UpdateStation.js
@@ -3,6 +3,9 @@ const util = require('util');
 
 const readFile = util.promisify(fs.readFile);
 
+const path = "./docs/routes/";
+let stationInfo = null;
+
 function exportNewData(fileName, newData) {
     fs.writeFile(fileName, JSON.stringify(newData), err => {
         if (err) {
@@ -117,6 +120,8 @@ function createStationData() {
             })
         });
         exportNewData('./docs/stationInfo.json', newStationData);
+        console.log("Waiting until saving of the stationInfo file is done");
+        setTimeout(loadAllTheNeededFiles, 5000, items.length);
     });
     
     function getTheRightTrainLine(traWebsiteCode) {
@@ -333,4 +338,83 @@ function sortRoundLine() {
         });
         exportNewData('./docs/Lines/RoundLine.json', data);
     });
+}
+
+function loadAllTheNeededFiles() {
+
+    async function getStationInfo() {
+        return await readFile('./docs/stationInfo.json');
+    }
+
+    getStationInfo().then(data => {
+        readDir(JSON.parse(data));
+    });
+
+    function readDir(infoAboutStations) {
+        stationInfo = infoAboutStations;
+        stationInfo.stations = resetStationInfo(stationInfo);
+
+        fs.readdir(path, function (err, items) {
+
+            for (let i = 0; i < items.length; i++) {
+                countTheNumberOfStopsPerStation(items[i]);
+            }
+
+            setTimeout(checkTheData, 15000, items.length);
+        });
+    }
+
+    function resetStationInfo(infoAboutStations) {
+        return infoAboutStations.stations.map(function (el) {
+            el.stops = 0;
+            return el;
+        })
+    }
+}
+
+function countTheNumberOfStopsPerStation(fileName) {
+
+    async function getJsonFile() {
+        return await readFile(path + fileName);
+    }
+
+    getJsonFile().then(jsonData => {
+        iterateOverTrainRouteStops(JSON.parse(jsonData));
+    });
+
+    function iterateOverTrainRouteStops(trainStopRoute) {
+        trainStopRoute.TrainInfos.forEach(function (el) {
+            el.TimeInfos.forEach(function (element) {
+                let index = stationInfo.stations.findIndex((sel => sel.時刻表編號 === parseInt(element.Station)));
+                stationInfo.stations[index].stops++;
+            })
+        });
+    }
+}
+
+function checkTheData(lengthOfItems) {
+
+    nowMakeTheStopsAverageBasedOnTheNumberOfFiles();
+
+    function nowMakeTheStopsAverageBasedOnTheNumberOfFiles() {
+        stationInfo.stations = stationInfo.stations.map(function (el) {
+            el.stops = Math.round(parseInt(el.stops) / lengthOfItems);
+            return el;
+        });
+        exportChangedStationInfo(stationInfo);
+    }
+
+    function exportChangedStationInfo(newData) {
+        fs.writeFile("./docs/stationInfo.json", JSON.stringify(newData), err => {
+            if (err) {
+                console.log('Error writing file', err)
+            } else {
+                console.log('Successfully wrote file')
+            }
+        });
+    }
+}
+
+function updateDifferentLineData() {
+
 }
