0c292fa62076509cf97ac3ff60e37b518b4f8e75
svend <sven.depickere@student.howest.be>
2019-10-07 11:25:55 +0200

    Create createCountyOfData.js

---
+++ createCountyOfData.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+
+function exportNewData(fileName, newData) {
+    fs.writeFile(fileName, JSON.stringify(newData), err => {
+        if (err) {
+            console.log('Error writing file', err)
+        } else {
+            console.log('Successfully wrote file')
+        }
+    })
+}
+
+alternateDataSoWeHaveCounties();
+
+function alternateDataSoWeHaveCounties() {
+
+    async function getStuff() {
+        return await readFile('車站基本資料集.json');
+    }
+
+    getStuff().then(data => {
+        data = JSON.parse(data);
+        data.features = data.map(function (el) {
+            el.縣市 = el.stationAddrTw.split(' ')[0];
+            return el;
+        });
+        exportNewData('車站基本資料集.json', data);
+    });
+}
