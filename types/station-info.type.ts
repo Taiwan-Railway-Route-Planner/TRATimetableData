f75cb3f324475c5b747c5a56b5b5d82bc9c899db
Depickere Sven <sven.depickere@hotmail.com>
2022-03-05 11:57:35 +0100

    feat(): Add stationInfo type

---
+++ types/station-info.type.ts
@@ -0,0 +1,24 @@
+export type StationInfo = Readonly<{
+  routeInfo: {
+    name: string;
+    code: number;
+  }[]
+  counties: {
+    縣市: string;
+    eng縣市: string
+  }[]
+  stations: {
+    時刻表編號: number;
+    traWebsiteCode: string;
+    站名: string;
+    eng站名: string;
+    routeCode: number[];
+    gradeStation: {
+      status: boolean;
+      value: number;
+    };
+    stops: number;
+    縣市: string;
+    eng縣市: string
+  }
+}>
