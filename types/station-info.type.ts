417a48666b276412611ce725cdc5c6bca7bcad50
Depickere Sven <sven.depickere@hotmail.com>
2022-03-05 13:33:01 +0100

    refactor(): Add new type

--- types/station-info.type.ts
+++ types/station-info.type.ts
@@ -7,18 +7,20 @@ export type StationInfo = Readonly<{
     縣市: string;
     eng縣市: string
   }[]
-  stations: {
-    時刻表編號: number;
-    traWebsiteCode: string;
-    站名: string;
-    eng站名: string;
-    routeCode: number[];
-    gradeStation: {
-      status: boolean;
-      value: number;
-    };
-    stops: number;
-    縣市: string;
-    eng縣市: string
-  }[]
+  stations: StationDetails[]
+}>
+
+export type StationDetails = Readonly<{
+  時刻表編號: number;
+  traWebsiteCode: string;
+  站名: string;
+  eng站名: string;
+  routeCode: number[];
+  gradeStation: {
+    status: boolean;
+    value: number;
+  };
+  stops: number;
+  縣市: string;
+  eng縣市: string
 }>
