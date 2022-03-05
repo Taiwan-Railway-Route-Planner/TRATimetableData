632a448e7cec95eef0f196093ee94e8b3093cdd2
Depickere Sven <sven.depickere@hotmail.com>
2022-03-05 13:58:23 +0100

    refactor(): Update types

--- types/train-info.type.ts
+++ types/train-info.type.ts
@@ -1,7 +1,7 @@
 import { Stations } from './stations.type';
 import { EnrichedTimeInfo, TimeInfo } from './time-info.type';
 
-export type TrainInfo = Readonly<{
+export type BaseTrainInfo = {
   Type:         string;
   Train:        string;
   BreastFeed:   string;
@@ -19,25 +19,13 @@ export type TrainInfo = Readonly<{
   Everyday:     string;
   Note:         string;
   NoteEng:      string;
+}
+
+export type TrainInfo = BaseTrainInfo & {
   TimeInfos:    TimeInfo[];
-}>
+}
 
-
-export type EnrichedTrainInfo = {
-  Type:         string;
-  Train:        string;
-  BreastFeed:   string;
-  Route:        string;
-  Package:      string;
-  OverNightStn: string;
-  LineDir:      string;
-  Line:         string;
-  Dinning:      string;
-  Cripple:      string;
-  CarClass:     string;
-  Bike:         string;
-  Note:         string;
-  NoteEng:      string;
+export type EnrichedTrainInfo = BaseTrainInfo & {
   TimeInfos:    EnrichedTimeInfo;
   StartStation: number;
   StartTime:    string;
